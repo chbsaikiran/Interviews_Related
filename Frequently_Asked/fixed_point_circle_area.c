7f2b1fdc37aecc31880d5ffe7386198783969aa8
chbsaikiran <sai.406@gmail.com>
2025-09-05 11:06:12 +0530

    changes to c file

--- Frequently_Asked/fixed_point_circle_area.c
+++ Frequently_Asked/fixed_point_circle_area.c
@@ -7,12 +7,12 @@ int main()
 	float radius = 4.67;
 	int area_fx;
 	float area,area_q;
-	short radius_fx = (short)((float)((4.67)*(((int)1) << 12)) + 0.5); //Q3.12
+	short radius_fx = (short)((float)((radius)*(((int)1) << 12)) + 0.5); //Q3.12
 	int temp = PI * radius_fx; //Q2.13 * Q3.12 -> Q5.25
 	short temp_sh = temp >> 15; //Q5.10
 	area_fx = temp_sh * radius_fx; //Q5.10 * Q3.12 -> Q8.22
 	area_q = (area_fx / ((float)(((int)1) << 22)));
-	area = (3.14159 * 4.67 * 4.67);
+	area = (3.14159 * radius * radius);
     printf("area float = %f, area fixed = %f, error = %f", area, area_q, (area - area_q));
 }
 
